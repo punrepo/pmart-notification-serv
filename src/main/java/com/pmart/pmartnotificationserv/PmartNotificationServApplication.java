61ea3b90eccde56aaf430e0ed042e5fedffa9c48
Puneet Pant <pmpant16@gmail.com>
2025-07-31 19:01:23 -0700

    initial commit

---
+++ src/main/java/com/pmart/pmartnotificationserv/PmartNotificationServApplication.java
@@ -0,0 +1,13 @@
+package com.pmart.pmartnotificationserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmartNotificationServApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmartNotificationServApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pmart/pmartnotificationserv/PmartNotificationServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmart.pmartnotificationserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmartNotificationServApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
